1902605a2d22a0cbb061fb7b79cadb79f2f9a32b
Bhawna <bhawnajune2006@gmail.com>
2026-06-26 22:20:57 +0530

    Time: 1 ms (52.13%), Space: 72.6 MB (76.28%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int max=arr[arr.size()-1];
+        for(int i=arr.size()-2;i>0;i--){
+            int temp=arr[i];
+            arr[i]=max;
+            if(temp>max){
+                max=temp;
+            }
+        }
+        arr[0]=max;
+        arr[arr.size()-1]=-1;
+        return arr;
+    }
+};
