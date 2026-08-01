cc992b085f8cf4fc0170ebcf8de620c0f20cb443
Bhawna <bhawnajune2006@gmail.com>
2026-08-02 02:05:05 +0530

    Time: 0 ms (100%), Space: 9.4 MB (99.74%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int t[23][23];
+    //Difference nikalne k liye
+    int solve(vector<int>&nums,int i,int j){
+        if(i>j){
+            return 0;
+        }
+        if(i==j){
+            return nums[i];
+        }
+        if(t[i][j]!=-1){
+            return t[i][j];
+        }
+        int take_i=nums[i]-solve(nums,i+1,j);
+        int take_j=nums[j]-solve(nums,i,j-1);
+        return t[i][j]=max(take_i,take_j);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        memset(t,-1,sizeof(t));
+        return solve(nums,0,n-1)>=0;
+    }
+};
