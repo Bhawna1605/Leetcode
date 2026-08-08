0798187b13e927bfbf0cbe22f08461c04e932e91
Bhawna <bhawnajune2006@gmail.com>
2026-08-08 22:21:28 +0530

    Time: 46 ms (54.26%), Space: 121.3 MB (13.83%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -2,26 +2,34 @@ class Solution {
 public:
     vector<int> validSequence(string word1, string word2) {
         int n = word1.length(), m = word2.length();
-        vector<int> last(m, -1);
-        int j = m - 1;
-        for (int i = n - 1; i >= 0; --i) {
-            if (j >= 0 && word1[i] == word2[j]) {
-                last[j] = i;
-                j -= 1;
+        vector<int>rightHandSideMatchLength(n,0);
+        int rightMatched=0;
+        int i=n-1;
+        int j =m-1;
+        while(i>=0){
+            if(j>=0 && word1[i]==word2[j]){
+                rightMatched++;
+                j--;
             }
+            rightHandSideMatchLength[i]=rightMatched;
+            i--;
         }
-        vector<int> res;
-        int skip = 0;
-        j = 0;
-        for (int i = 0; i < n; ++i) {
-            if (j == m) break;
-            if (word1[i] == word2[j] ||
-                (skip == 0 && (j == m - 1 || i < last[j + 1]))) {
-                skip += (word1[i] != word2[j] ? 1 : 0);
-                res.push_back(i);
-                j += 1;
+        vector<int>seq;
+        bool changePower=true;//can change only one character
+        i=0;
+        j=0;
+        while(i<n && j<m){
+            if(word1[i]==word2[j]){
+                seq.push_back(i);
+                j++;
             }
+            else if(changePower==true && i+1<n && rightHandSideMatchLength[i+1]>=m-j-1){
+                seq.push_back(i);
+                j++;
+                changePower=false;
+            }
+            i++;
         }
-        return j == m ? res : vector<int>();
+        return j==m?seq:vector<int>();
     }
 };
