11934bc1af1b92e9cd7a6393dc87bcd454ad2255
Bhawna <bhawnajune2006@gmail.com>
2026-08-19 23:26:00 +0530

    Time: 16 ms (85.98%), Space: 60.1 MB (77.36%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        unordered_map<int,int>mp;//row->seats in each row booked
+        for(auto& reservedSeat:reservedSeats){
+            int row=reservedSeat[0];
+            int seat=reservedSeat[1];
+            mp[row] |=(1<<seat);
+        }
+        int result=(n-mp.size())*2;
+        int maskA=(1<<2) | (1<<3) | (1<<4) |(1<<5);
+        int maskB=(1<<4) | (1<<5) | (1<<6) |(1<<7);
+        int maskC=(1<<6) | (1<<7) | (1<<8) |(1<<9);
+        for(auto& [row,bookedSeatsMask]:mp){
+            bool grpA=(bookedSeatsMask & maskA)==0;
+            bool grpB=(bookedSeatsMask & maskB)==0;
+            bool grpC=(bookedSeatsMask & maskC)==0;
+            if(grpA && grpC){
+                result+=2;
+            }
+            else if(grpA || grpB || grpC){
+                result+=1;
+            }
+        }
+        return result;
+    }
+};
