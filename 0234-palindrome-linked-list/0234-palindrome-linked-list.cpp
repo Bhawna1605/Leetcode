655a2706a97a59dc339a0debedec0c1d886f9604
Bhawna <bhawnajune2006@gmail.com>
2026-09-27 00:14:47 +0530

    Time: 7 ms (26.58%), Space: 126.7 MB (28.43%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    ListNode* reverse(ListNode* head) {
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        while (curr != NULL) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+    ListNode* curr;
+    bool solve(ListNode* head) {
+        if(!head)
+            return true;
+        if(!solve(head->next) || head->val != curr->val) {
+            return false;
         }
-        return prev;
+        curr = curr->next;
+        return true;
     }
     bool isPalindrome(ListNode* head) {
-        if (head == NULL || head->next == NULL)
-            return true;
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        ListNode* secondHalf = reverse(slow);
-        ListNode* firstHalf = head;
-        while (secondHalf != NULL) {
-            if (firstHalf->val != secondHalf->val)
-                return false;
-            firstHalf = firstHalf->next;
-            secondHalf = secondHalf->next;
-        }
-        return true;
+        curr = head;
+        return solve(head);
     }
 };
